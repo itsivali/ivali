59f1e8359d41c810b106aaf50f4849cea53018e5
Willis Ivali <itsivali@outlook.com>
2025-03-19 20:36:51 +0300

    Refactor skills progress bar for improved layout and animation

--- assets/js/skillscounter.js
+++ assets/js/skillscounter.js
@@ -1,43 +1,41 @@
 function skillsProgress() {
-    var skills = {
-      "terraform": "85%",
-      "python": "90%",
-      "go": "80%",
-      "docker": "75%",
-      "kubernetes": "70%",
-      "flutter": "80%"
-    };
-  
-    var multiply = 2;
-    var delay = 600;
-  
-    $.each(skills, function(language, targetPercent) {
-      setTimeout(function() {
-        $('#progress-' + language).animate(
-          { 'width': targetPercent },
-          {
-            duration: 1000, // 1 second animation
-            step: function(now) {
-              $('#progress-text-' + language).css({ 'font-weight': 'bold', 'color': 'red' });
-              $('#progress-text-' + language).text(Math.floor(now) + "%");
-            },
-            complete: function() {
-              $('#progress-text-' + language).css({ 'font-weight': 'bold', 'color': 'red' });
-              $('#progress-text-' + language).text(targetPercent);
-            }
+  var skills = {
+    "terraform": "85%",
+    "python": "90%",
+    "go": "80%",
+    "docker": "75%",
+    "kubernetes": "70%",
+    "flutter": "80%"
+  };
+
+  var multiply = 2;
+  var delay = 600;
+
+  $.each(skills, function (language, targetPercent) {
+    setTimeout(function () {
+      $('#progress-' + language).animate(
+        { 'width': targetPercent },
+        {
+          duration: 1000, // 1 second animation
+          step: function (now) {
+            $(this).text(Math.floor(now) + "%");
+          },
+          complete: function () {
+            $(this).text(targetPercent);
           }
-        );
-      }, delay * multiply);
-      multiply++;
-    });
-  }
-  
+        }
+      );
+    }, delay * multiply);
+    multiply++;
+  });
+}
+
+if ($(window).scrollTop() >= $("#about").offset().top - 200) {
+  skillsProgress();
+}
+
+$(window).on("scroll", function () {
   if ($(window).scrollTop() >= $("#about").offset().top - 200) {
     skillsProgress();
   }
-  
-  $(window).on("scroll", function() {
-    if ($(window).scrollTop() >= $("#about").offset().top - 200) {
-      skillsProgress();
-    }
-  });
+});
